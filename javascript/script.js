e85778be09ec8d01e141a733d710fa80915227c5
Vouyeuz <fahrizal.indi@gmail.com>
2021-07-07 08:16:32 +0700

    playing around

--- javascript/script.js
+++ javascript/script.js
@@ -1,41 +1,66 @@
-function Mahasiswa(nama, energi) {
-  let mahasiswa = {};
-  mahasiswa.nama = nama;
-  mahasiswa.energi = energi;
+// function Mahasiswa(nama, energi) {
+//   let mahasiswa = {};
+//   mahasiswa.nama = nama;
+//   mahasiswa.energi = energi;
 
-  mahasiswa.makan = function (porsi) {
-    this.energi += porsi;
-    console.log(`Halo ${this.nama}, selamat makan!`);
-  };
+//   mahasiswa.makan = function (porsi) {
+//     this.energi += porsi;
+//     console.log(`Halo ${this.nama}, selamat makan!`);
+//   };
 
-  mahasiswa.tidur = function (bobo) {
-    this.energi -= bobo * 10 + 5;
-    console.log(`Met bobo ya, ${this.nama}.`);
-  };
+//   mahasiswa.tidur = function (bobo) {
+//     this.energi -= bobo * 10 + 5;
+//     console.log(`Met bobo ya, ${this.nama}.`);
+//   };
 
-  return mahasiswa;
+//   return mahasiswa;
+// }
+
+// let fahrizal = Mahasiswa("Fahrizal", 100);
+// let indi = Mahasiswa("Indi", 200);
+// let dirgantara = Mahasiswa("Dirgantara", 300);
+
+// // simple calculator
+
+// function calculator(a,b,sign){
+//   if ((typeof a === "number") && (typeof b === "number")) {
+//     switch (sign) {
+//     case "+":
+//       return a + b;
+//     case "-":
+//       return a - b;
+//     case "*":
+//       return a * b;
+//     case "/":
+//       return a / b;
+//     }
+//   }
+//   console.log(calculator);
+//   return "unknown value";
+// }
+
+//hoisting, scope, execution phase
+
+// function introduceYourSelf() {
+//   return `Hi, my name is ${name}, a regular ${age} years old ${gender}. I am a proud ${religion}.`;
+// }
+// const name = "Fahrizal Indi Dirgantara";
+// const age = [27, 24];
+// const gender = "Male";
+// const religion = "Islam";
+// console.log(introduceYourSelf());
+var nama = "Fahrizal";
+function satu() {
+  // var nama = "Fahrizal";
+  console.log(nama);
 }
 
-let fahrizal = Mahasiswa("Fahrizal", 100);
-let indi = Mahasiswa("Indi", 200);
-let dirgantara = Mahasiswa("Dirgantara", 300);
-
-
-// simple calculator
-
-function calculator(a,b,sign){
-  if ((typeof a === "number") && (typeof b === "number")) {
-    switch (sign) {
-    case "+":
-      return a + b;
-    case "-":
-      return a - b;
-    case "*":
-      return a * b;
-    case "/":
-      return a / b;
-    }
-  }
-  console.log(calculator);
-  return "unknown value";
+function dua() {
+  console.log(nama);
 }
+
+console.log(nama);
+var nama = "Indi";
+satu();
+dua("Dirgantara");
+console.log(nama);
