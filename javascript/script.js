2cf18a5a392bbd750bda5a002e4cd18111b8f9c4
Vouyeuz <fahrizal.indi@gmail.com>
2021-07-22 11:24:40 +0700

    online test

--- javascript/script.js
+++ javascript/script.js
@@ -49,18 +49,64 @@
 // const gender = "Male";
 // const religion = "Islam";
 // console.log(introduceYourSelf());
-var nama = "Fahrizal";
-function satu() {
-  // var nama = "Fahrizal";
-  console.log(nama);
-}
+// var nama = "Fahrizal";
+// function satu() {
+//   // var nama = "Fahrizal";
+//   console.log(nama);
+// }
 
-function dua() {
-  console.log(nama);
-}
+// function dua() {
+//   console.log(nama);
+// }
 
-console.log(nama);
-var nama = "Indi";
-satu();
-dua("Dirgantara");
-console.log(nama);
+// console.log(nama);
+// var nama = "Indi";
+// satu();
+// dua("Dirgantara");
+// console.log(nama);
+
+// KeDa Tech: Programming Test
+
+// for x in range(1,101):
+//     if x % 15 == 0:
+//         print "FizzBuzz" # Catch multiples of both first.
+//     elif x % 3 == 0:
+//         print "Fizz"
+//     elif x % 5 == 0:
+//         print "Buzz"
+//     else:
+//         print x
+
+// console.log(x);
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 15 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 12 === 0) {
+//     console.log("HetDah");
+//   } else if (i % 3 === 0) {
+//     console.log("Het");
+//   } else if (i % 4 === 0) {
+//     console.log("Dah");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+let beads = [1, 2, 3, 4, 5, 6, 7];
+
+beads.push(8, "end");
+beads.unshift("start");
+console.log(beads);
+beads.pop();
+console.log(beads);
