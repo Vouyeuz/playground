d721f4313b10eafdb37f309a28db3a16fbf01b64
Vouyeuz <fahrizal.indi@gmail.com>
2021-09-21 14:53:09 +0700

    login page

--- reactjs/src/Components/LoginContainer/LoginContainer.js
+++ reactjs/src/Components/LoginContainer/LoginContainer.js
@@ -1,21 +1,51 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-export class LoginContainer extends Component {
-    constructor(props) {
-        super(props)
+// export class LoginContainer extends Component {
+//     constructor(props) {
+//         super(props)
     
-        this.state = {
+//         this.state = {
              
-        }
-    }
+//         }
+//     }
     
-    render() {
-        return (
-            <div>
-             login   
-            </div>
-        )
-    }
+//     render() {
+//         return (
+//             <div>
+//              login   
+//             </div>
+//         )
+//     }
+// }
+
+// export default LoginContainer
+
+
+import React from 'react';
+import { Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+
+const LoginContainer = () => {
+  return (
+    <Form>
+      <FormGroup row>
+        <Label className= "font-weight-bold" for="LoginContainerEmail">Email</Label>
+        <Col>
+          <Input type="email" name="email" id="LoginContainerEmail" placeholder="user@domain.com" />
+        </Col>
+      </FormGroup>
+      <FormGroup row className= "py-2">
+        <Label for="LoginContainerPassword">Password</Label>
+        <Col>
+          <Input type="password" name="password" id="LoginContainerPassword" placeholder="********" />
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Col className= "py-2">
+          <Button>Login</Button>
+        </Col>
+      </FormGroup>
+    </Form>
+  );
 }
 
-export default LoginContainer
+export default LoginContainer;
--- reactjs/src/Pages/LoginPage/LoginPage.js
+++ reactjs/src/Pages/LoginPage/LoginPage.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
-import { LoginContainer } from '../../Components';
-
+import React, { Component } from "react";
+import { Card } from "reactstrap";
+import { LoginContainer } from "../../Components";
 
 export class LoginPage extends Component {
-    render() {
-        return (
-            <div>
-                <LoginContainer />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div style={{position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}}>
+        <Card className="p-2 border-rounded-5 bg-primary">
+          <LoginContainer />
+        </Card>
+      </div>
+    );
+  }
 }
 
-export default LoginPage
+export default LoginPage;
