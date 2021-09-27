7b5c747a8747e26dbc6bf712af8a289cb5161ae9
Vouyeuz <fahrizal.indi@gmail.com>
2021-09-27 08:30:24 +0700

    search bar design

--- reactjs/src/Components/Header/Header.js
+++ reactjs/src/Components/Header/Header.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Card } from "reactstrap";
 
 export class Header extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             
-        }
-    }
-    
-    render() {
-        return (
-            <div>
-             header   
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Card className="pb-1 m-2 shadow bg-primary">
+          <h3 className="m-3">
+            <strong className="text-white">GitHub </strong>Jobs
+          </h3>
+        </Card>
+      </div>
+    );
+  }
 }
 
-export default Header
+export default Header;
--- reactjs/src/Pages/LoginPage/LoginPage.js
+++ reactjs/src/Pages/LoginPage/LoginPage.js
@@ -6,7 +6,10 @@ export class LoginPage extends Component {
   render() {
     return (
       <div style={{position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}}>
-        <Card className="p-2 border-rounded-5 bg-primary">
+        <Card className="p-2 border-rounded border-dark bg-gradient bg-secondary">
+        <Card className="pt-3 mb-4 border-rounded text-center bg-gradient bg-primary">
+          <p style={{fontSize: "1.4rem"}}><strong>GitHub</strong> Jobs</p>
+        </Card>
           <LoginContainer />
         </Card>
       </div>
